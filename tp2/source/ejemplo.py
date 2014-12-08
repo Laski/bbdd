72ca8f9d362a9197e6628a97460b4e9aba7c7379
Laski <laski.nahuel@gmail>
2014-12-08 07:36:44 -0300

    DistributionSteps empezado. Faltan casos borde.

--- tp2/source/ejemplo.py
+++ tp2/source/ejemplo.py
@@ -10,3 +10,9 @@ aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parametro
 print "Classic Histogram"
 print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
 print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
+
+# DistributionSteps
+aEstimator = estimators.DistributionSteps('db.sqlite3', 'table1', 'c1', parametro=20)
+print "DistributionSteps"
+print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
--- tp2/source/estimators.py
+++ tp2/source/estimators.py
@@ -6,6 +6,7 @@ import random
 
 from interfazbd import InterfazBD
 
+
 def validar(db, tabla, columna):
     def nombres_columnas(columnas):
         return [t[1] for t in columnas]
@@ -21,9 +22,9 @@ def validar(db, tabla, columna):
             return
     assert False, "La tabla no existe o no tiene la columna especificada"
     
+    
 class Estimador(object):
     """Clase base de los estimadores."""
-
     def __init__(self, db, tabla, columna, parametro=10):
         validar(db, tabla, columna)
         self.db = InterfazBD(db)
@@ -42,7 +43,9 @@ class Estimador(object):
     def estimate_greater(self, value):
         raise NotImplementedError()
 
-class ClassicHistogram(Estimador):  #Estimador de Piatetsky-Shapiro basado en histogramas clásicos
+
+class ClassicHistogram(Estimador):
+    """Estimador de Piatetsky-Shapiro basado en histogramas clásicos."""
     def build_struct(self):
         consulta_minmax = "SELECT %s(" + self.columna + ") FROM (SELECT " + self.columna + " FROM " \
                                        + self.tabla + " GROUP BY " + self.columna + ")"
@@ -69,25 +72,53 @@ class ClassicHistogram(Estimador):  #Estimador de Piatetsky-Shapiro basado en hi
 
     def estimate_greater(self, valor):
         bucket = self.ubicar_valor(valor)
-        minima = sum(self.probabilidades[bucket+1:])    # la probabilidad minima es la de todos los buckets mayores
-        maxima = sum(self.probabilidades[bucket:])      # la probabilidad maxima es la de todos los buckets mayores o iguales
-        return (minima + maxima)/2                      # tomo el promedio como sugiere el paper
+        probabilidad_minima = sum(self.probabilidades[bucket+1:])    # la probabilidad minima es la de todos los buckets mayores sumados
+        probabilidad_maxima = sum(self.probabilidades[bucket:])      # la probabilidad maxima es la de todos los buckets mayores o iguales sumados
+        return (probabilidad_minima + probabilidad_maxima)/2         # tomo el promedio como sugiere el paper
 
     def ubicar_valor(self, valor):
         return int((valor - self.minimo) / self.longitud_bucket)
 
 
-class DistributionSteps(Estimador): #Estimador de Piatetsky-Shapiro basado en Distribution Steps
+class DistributionSteps(Estimador):
+    """Estimador de Piatetsky-Shapiro basado en Distribution Steps."""
     def build_struct(self):
-        pass
+        consulta_cantidad = "SELECT COUNT(" + self.columna + ") FROM " + self.tabla
+        self.cantidad = list(self.db.realizar_consulta(consulta_cantidad))[0][0]
+
+        self.altura_bucket = self.cantidad/self.parametro
+        self.separadores = [1+i*self.altura_bucket for i in range(self.parametro)]    # self.separadores tiene las "posiciones" mencionadas en el paper, los puntos de corte
+        self.separadores.append(self.cantidad)   # la ultima posicion debe ser siempre la cantidad total de filas, sin importar las cuestiones de redondeo
+        print self.separadores
+        self.bordes = []    # la idea es que self.bordes[i] = STEP(i). es decir, que tenga los valores limite de cada bucket del histograma
+        consulta_ordenada = "SELECT " + self.columna + " FROM " + self.tabla + " ORDER BY " + self.columna + " ASC"
+        i = 0
+        for valor in self.db.realizar_consulta(consulta_ordenada):
+            i += 1  # voy contando la cantidad de registros que veo
+            if i in self.separadores:   # estoy en uno de los límites?
+                # tengo un nuevo bucket
+                valor = valor[0]    # desempaqueto
+                self.bordes.append(valor)
+        print (self.bordes) #debug
+        self.probabilidad_por_bucket = 1.0 / self.parametro
 
     def estimate_equal(self, valor):
-        pass
+        return 0.5/self.parametro     # no depende del valor
 
     def estimate_greater(self, valor):
-        pass
+        bucket = self.ubicar_valor(valor)
+        buckets_mayores = self.parametro - bucket - 1
+        buckets_mayores_o_iguales = self.parametro - bucket
+        probabilidad_minima = self.probabilidad_por_bucket * buckets_mayores
+        probabilidad_maxima = self.probabilidad_por_bucket * buckets_mayores_o_iguales
+        return (probabilidad_minima + probabilidad_maxima)/2
 
-class EstimadorPropio(Estimador):
+    def ubicar_valor(self, valor):
+        limite_inferior = max([limite for limite in self.bordes if limite <= valor])
+        return self.bordes.index(limite_inferior)
+
+
+class EstimadorGrupo(Estimador):
     def build_struct(self):
         pass
 

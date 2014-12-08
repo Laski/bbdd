95c03d36edfae3e08d40107b05cffd3bc175d7cb
Laski <laski.nahuel@gmail>
2014-12-08 05:34:45 -0300

    La validación ahora es con assert

--- tp2/source/estimators.py
+++ tp2/source/estimators.py
@@ -6,38 +6,33 @@ import pylab
 import random
 
 
-def validar(db,tabla,columna):
-	def nombres_columnas(columnas):
-		return [t[1] for t in columnas]
-	def tipos_columnas(columnas):
-		return [t[2] for t in columnas]
+def validar(db, tabla, columna):
+    def nombres_columnas(columnas):
+        return [t[1] for t in columnas]
+    def tipos_columnas(columnas):
+        return [t[2] for t in columnas]
 
-	db = InterfazBD(db)
-	res = db.listar_tablas()
-	for i in range(0,len(res),2):
-		if res[i] == tabla and columna in nombres_columnas(res[i+1]):
-			index = nombres_columnas(res[i+1]).index(columna)
-			return 'int' == tipos_columnas(res[i+1])[index] 
-	return False
- 	
+    db = InterfazBD(db)
+    tablas = db.listar_tablas()
+    for i in range(0, len(tablas) ,2):
+        if tablas[i] == tabla and columna in nombres_columnas(tablas[i+1]):
+            index_columna = nombres_columnas(tablas[i+1]).index(columna)
+            assert 'int' == tipos_columnas(tablas[i+1])[index_columna], "La columna no es de tipo entero"
+            return
+    assert False, "La tabla no existe o no tiene la columna especificada"
+    
 class Estimador(object):
     """Clase base de los estimadores."""
 
     def __init__(self, db, tabla, columna, parametro=10):
-        ''' Laski, si querés hacerlo con try,except, mejor.. Yo no tengo muy claro como laburar con eso '''
-
-        if  validar(db,tabla,columna):
-	        self.db = db
-	        self.tabla = tabla
-	        self.columna = columna
-	        self.parametro = parametro
-
-    		# Construye las estructuras que necesita el estimador.
-       		self.build_struct()
-       	else:
-       		print("La Tabla/Columna ingresada no existe o el tipo no es INT")
-       		exit()
-
+        validar(db, tabla, columna)
+        self.db = db
+        self.tabla = tabla
+        self.columna = columna
+        self.parametro = parametro
+        # Construye las estructuras que necesita el estimador.
+        self.build_struct()
+        
     def build_struct(self):
         raise NotImplementedError()
 
@@ -47,59 +42,59 @@ class Estimador(object):
     def estimate_greater(self, value):
         raise NotImplementedError()
 
-class ClassicHistogram(Estimador):	#Estimador de Piatetsky-Shapiro basado en histogramas clásicos
-	def build_struct(self):
-		self.db = InterfazBD(self.db)
-		consulta_minmax = "SELECT %s(" + self.columna + ") FROM (SELECT " + self.columna + " FROM " \
-									   + self.tabla + " GROUP BY " + self.columna + ")"
-		self.minimo = list(self.db.realizar_consulta(consulta_minmax % "MIN"))[0][0]
-		self.maximo = list(self.db.realizar_consulta(consulta_minmax % "MAX"))[0][0]
+class ClassicHistogram(Estimador):  #Estimador de Piatetsky-Shapiro basado en histogramas clásicos
+    def build_struct(self):
+        self.db = InterfazBD(self.db)
+        consulta_minmax = "SELECT %s(" + self.columna + ") FROM (SELECT " + self.columna + " FROM " \
+                                       + self.tabla + " GROUP BY " + self.columna + ")"
+        self.minimo = list(self.db.realizar_consulta(consulta_minmax % "MIN"))[0][0]
+        self.maximo = list(self.db.realizar_consulta(consulta_minmax % "MAX"))[0][0]
 
-		self.longitud_bucket = (self.maximo - self.minimo) / float(self.parametro)
+        self.longitud_bucket = (self.maximo - self.minimo) / float(self.parametro)
 
-		self.bordes = [i*self.longitud_bucket for i in range(self.parametro+1)]
-		
-		hist = [0 for i in range(self.parametro+1)]
-		tot = 0
+        self.bordes = [i*self.longitud_bucket for i in range(self.parametro+1)]
+        
+        hist = [0 for i in range(self.parametro+1)]
+        tot = 0
 
-		for valor in self.db.consultar(self.tabla, self.columna):
-			valor = list(valor)[0]
-			bucket = self.ubicar_valor(valor)
-			hist[bucket] += 1
-			tot += 1
-		self.probabilidades = [hist_value / float(tot) for hist_value in hist]
-		print(self.probabilidades)
+        for valor in self.db.consultar(self.tabla, self.columna):
+            valor = list(valor)[0]
+            bucket = self.ubicar_valor(valor)
+            hist[bucket] += 1
+            tot += 1
+        self.probabilidades = [hist_value / float(tot) for hist_value in hist]
+        print(self.probabilidades)
 
-	def estimate_equal(self, valor):
-		bucket = self.ubicar_valor(valor)
-		return self.probabilidades[bucket]
+    def estimate_equal(self, valor):
+        bucket = self.ubicar_valor(valor)
+        return self.probabilidades[bucket]
 
-	def estimate_greater(self, valor):
-		bucket = self.ubicar_valor(valor)
-		self.minimo = sum(self.probabilidades[:bucket])
-		self.maximo = self.minimo + self.probabilidades[bucket]
-		return (self.minimo + self.maximo)/2
+    def estimate_greater(self, valor):
+        bucket = self.ubicar_valor(valor)
+        self.minimo = sum(self.probabilidades[:bucket])
+        self.maximo = self.minimo + self.probabilidades[bucket]
+        return (self.minimo + self.maximo)/2
 
-	def ubicar_valor(self, valor):
-		return int( (valor - self.minimo) / self.longitud_bucket)
+    def ubicar_valor(self, valor):
+        return int( (valor - self.minimo) / self.longitud_bucket)
 
-class DistributionSteps(Estimador):	#Estimador de Piatetsky-Shapiro basado en Distribution Steps
-	def build_struct(self):
-		pass
+class DistributionSteps(Estimador): #Estimador de Piatetsky-Shapiro basado en Distribution Steps
+    def build_struct(self):
+        pass
 
-	def estimate_equal(self, valor):
-		pass
+    def estimate_equal(self, valor):
+        pass
 
-	def estimate_greater(self, valor):
-		pass
+    def estimate_greater(self, valor):
+        pass
 
 class EstimadorPropio(Estimador):
-	def build_struct(self):
-		pass
+    def build_struct(self):
+        pass
 
-	def estimate_equal(self, valor):
-		pass
+    def estimate_equal(self, valor):
+        pass
 
-	def estimate_greater(self, valor):
-		pass
+    def estimate_greater(self, valor):
+        pass
 

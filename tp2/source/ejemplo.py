579a733ac9ed997076f7edd766e84a669332869b
Laski <laski.nahuel@gmail>
2014-12-10 00:52:57 -0300

    Terminado estimador DistributionSteps y agregado estimador perfecto

--- tp2/source/ejemplo.py
+++ tp2/source/ejemplo.py
@@ -2,17 +2,20 @@
 
 import estimators
 
-# Creo una instancia de la clase que representa al metodo
-# 'Histograma Clasico'
+# ClassicHistogram
 aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parametro=20)
-
-# Pruebo distintas instancias de estimacion
-print "Classic Histogram"
+print "ClassicHistogram"
 print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
 print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
 
 # DistributionSteps
 aEstimator = estimators.DistributionSteps('db.sqlite3', 'table1', 'c1', parametro=20)
 print "DistributionSteps"
+print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
+
+# Perfecto
+aEstimator = estimators.EstimadorPerfecto('db.sqlite3', 'table1', 'c1', parametro=20)
+print "EstimadorPerfecto"
 print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
 print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
--- tp2/source/estimators.py
+++ tp2/source/estimators.py
@@ -31,7 +31,18 @@ class Estimador(object):
         self.tabla = tabla
         self.columna = columna
         self.parametro = parametro
-        # Construye las estructuras que necesita el estimador.
+
+        # consultas útiles
+        self.consulta_desordenada = "SELECT " + self.columna + " FROM " + self.tabla
+        self.consulta_ordenada = "SELECT " + self.columna + " FROM " + self.tabla + " ORDER BY " + self.columna + " ASC"
+        self.consulta_min_max = "SELECT %s(" + self.columna + ") FROM (SELECT " + self.columna + " FROM " \
+                                + self.tabla + " GROUP BY " + self.columna + ")"
+        self.consulta_cantidad_tuplas = "SELECT COUNT(" + self.columna + ") FROM " + self.tabla
+        self.consulta_cuenta_ocurrencias = "SELECT " + self.columna + ", COUNT(*) FROM " + self.tabla + " GROUP BY " + self.columna
+        # valores utiles
+        self.n_registros = list(self.db.realizar_consulta(self.consulta_cantidad_tuplas))[0][0]
+        
+        # inicializo las estructuras
         self.build_struct()
         
     def build_struct(self):
@@ -47,76 +58,156 @@ class Estimador(object):
 class ClassicHistogram(Estimador):
     """Estimador de Piatetsky-Shapiro basado en histogramas clásicos."""
     def build_struct(self):
-        consulta_minmax = "SELECT %s(" + self.columna + ") FROM (SELECT " + self.columna + " FROM " \
-                                       + self.tabla + " GROUP BY " + self.columna + ")"
-        self.minimo = list(self.db.realizar_consulta(consulta_minmax % "MIN"))[0][0]
-        self.maximo = list(self.db.realizar_consulta(consulta_minmax % "MAX"))[0][0]
+        self.minimo = list(self.db.realizar_consulta(self.consulta_min_max % "MIN"))[0][0]    # hago la consulta por el minimo y la desempaqueto
+        self.maximo = list(self.db.realizar_consulta(self.consulta_min_max % "MAX"))[0][0]    # idem para maximo
 
         self.longitud_bucket = (self.maximo - self.minimo) / float(self.parametro)
 
-        self.bordes = [i*self.longitud_bucket for i in range(self.parametro+1)]     # self.bordes tiene los limites de cada bucket
-        
         hist = [0 for i in range(self.parametro+1)]     # inicializo el histograma
-        tot = 0     # para ir contando cantidad de registros (se usa para normalizar después)
         for valor in self.db.consultar(self.tabla, self.columna):
-            valor = list(valor)[0]
-            bucket = self.ubicar_valor(valor)
+            valor = list(valor)[0]  # desmpaqueto
+            bucket = self.ubicar(valor)
             hist[bucket] += 1
-            tot += 1
-        self.probabilidades = [hist_value / float(tot) for hist_value in hist]  # normalizamos el histograma
-        # self.probabilidades tiene, para cada bucket, la probabilidad de que un valor de la distribución en ese rango
+        self.probabilidades = [hist_value/float(self.n_registros) for hist_value in hist]  # normalizamos el histograma
+        # self.probabilidades tiene, para cada bucket, la probabilidad de que un valor de la distribución caiga en ese rango
 
     def estimate_equal(self, valor):
-        bucket = self.ubicar_valor(valor)
+        # simplemente devuelvo el valor del bucket en el histograma normalizado
+        try:
+            bucket = self.ubicar(valor)
+        except IndexError:
+            return 0    # si el valor está fuera del rango, la probabilidad de que uno sea igual a el es 0
         return self.probabilidades[bucket]
 
     def estimate_greater(self, valor):
-        bucket = self.ubicar_valor(valor)
-        probabilidad_minima = sum(self.probabilidades[bucket+1:])    # la probabilidad minima es la de todos los buckets mayores sumados
-        probabilidad_maxima = sum(self.probabilidades[bucket:])      # la probabilidad maxima es la de todos los buckets mayores o iguales sumados
-        return (probabilidad_minima + probabilidad_maxima)/2         # tomo el promedio como sugiere el paper
+        try:
+            bucket = self.ubicar(valor)
+        except IndexError:
+            return int(valor < self.minimo) # hacky pero anda: si el valor es menor que el minimo la probabilidad es 1,
+                                            # sino es mayor que el máximo y entonces es 0
+        probabilidad_minima = sum(self.probabilidades[bucket+1:])    # la probabilidad mínima es la de todos los buckets mayores sumados
+        probabilidad_maxima = sum(self.probabilidades[bucket:])      # la probabilidad máxima es la de todos los buckets mayores o iguales sumados
+        return (probabilidad_minima + probabilidad_maxima)/2         # tomo el promedio entre ambas, como sugiere el paper
 
-    def ubicar_valor(self, valor):
-        return int((valor - self.minimo) / self.longitud_bucket)
+    def ubicar(self, valor):
+        if valor < self.minimo or valor > self.maximo:
+            raise IndexError    # el valor está fuera del rango
+        return int((valor-self.minimo) / self.longitud_bucket)       # raro pero creanme que anda
 
 
 class DistributionSteps(Estimador):
-    """Estimador de Piatetsky-Shapiro basado en Distribution Steps."""
+    """
+        Estimador de Piatetsky-Shapiro basado en Distribution Steps.
+        Elegimos la versión que minimiza el error en el caso promedio.
+    """
     def build_struct(self):
-        consulta_cantidad = "SELECT COUNT(" + self.columna + ") FROM " + self.tabla
-        self.cantidad = list(self.db.realizar_consulta(consulta_cantidad))[0][0]
-
-        self.altura_bucket = self.cantidad/self.parametro
-        self.separadores = [1+i*self.altura_bucket for i in range(self.parametro)]    # self.separadores tiene las "posiciones" mencionadas en el paper, los puntos de corte
-        self.separadores.append(self.cantidad)   # la ultima posicion debe ser siempre la cantidad total de filas, sin importar las cuestiones de redondeo
+        self.altura_bucket = self.n_registros / self.parametro                        # la altura ideal de todos los buckets
+        self.separadores = [1+i*self.altura_bucket for i in range(self.parametro)]    # self.separadores tiene las "posiciones" de corte en el arreglo de registros
+        self.separadores.append(self.n_registros)   # la ultima posicion debe ser siempre la cantidad total de filas, sin importar las cuestiones de redondeo
         
-        self.bordes = []    # queremos self.bordes[i] = STEP(i). es decir, que tenga los valores limite de cada bucket del histograma
-        consulta_ordenada = "SELECT " + self.columna + " FROM " + self.tabla + " ORDER BY " + self.columna + " ASC"
+        self.bordes = []    # queremos self.bordes[i] = lo que el paper llama STEP(i). es decir, que tenga los valores limite de cada bucket del histograma.
         i = 0
-        for valor in self.db.realizar_consulta(consulta_ordenada):
+        for (valor, ) in self.db.realizar_consulta(self.consulta_ordenada):
             i += 1  # voy contando la cantidad de registros que veo
             if i in self.separadores:   # estoy en uno de los límites?
-                # tengo un nuevo bucket
-                valor = valor[0]    # desempaqueto
+                # inauguro un nuevo bucket
                 self.bordes.append(valor)
-        self.probabilidad_por_bucket = 1.0 / self.parametro
+        # computamos delta
+        self.delta = min(0.5/self.parametro, self.get_density())   # extraido del paper
 
+    def get_density(self):
+        acum = sum([ocurrencias**2
+                    for valor, ocurrencias in self.db.realizar_consulta(self.consulta_cuenta_ocurrencias)
+                    if self.bordes.count(valor < 2)])   # extraido del paper
+        return acum / float((self.n_registros**2))      # idem
+        
+    def es_extremo(self, valor):
+        return valor == self.bordes[0] or valor == self.bordes[-1]
 
-    # TODO: hay que elegir si queremos minimizar el error en el peor caso o minimizar el error promedio. lo primero parece más sencillo.
+    def entre_bordes(self, valor):
+        # caso A
+        return self.bordes.count(valor) == 0
+    
+    def igual_a_uno_no_extremo(self, valor):
+        # caso B1
+        return self.bordes.count(valor) == 1 and not self.es_extremo(valor)
+
+    def igual_a_varios_no_extremos(self, valor):
+        # caso B2
+        return self.bordes.count(valor) > 1 and not self.es_extremo(valor)
+
+    def igual_a_varios_incluyendo_extremos(self, valor):
+        # caso C
+        return self.bordes.count(valor) > 1 and self.es_extremo(valor)
+
+    def igual_a_un_extremo(self, valor):
+        # caso C1
+        return self.bordes.count(valor) == 1 and self.es_extremo(valor)
+
+    def fuera_del_histograma(self, valor):
+        # caso D
+        return valor < self.bordes[0] or valor > self.bordes[-1]
+
     def estimate_equal(self, valor):
-        return 0.5/self.parametro     # no depende del valor   #TODO: ver casos borde
+        # extraido del paper
+        if self.entre_bordes(valor) or self.igual_a_uno_no_extremo(valor):
+            # caso A y B1
+            return self.delta
+        if self.igual_a_varios_no_extremos(valor):
+            # caso B2
+            return self.bordes.count(valor) / self.parametro
+        if self.igual_a_varios_incluyendo_extremos(valor):
+            # caso C
+            if valor == self.bordes[0] == self.bordes[-1]:
+                return 1    # todos son iguales
+            return (self.bordes.count(valor)-0.5) / self.parametro
+        if self.igual_a_un_extremo(valor):
+            # caso C1
+            return self.delta / 2
+        if self.fuera_del_histograma(valor):
+            # caso D
+            return 0        # ninguno puede ser igual a algo que cae fuera
 
     def estimate_greater(self, valor):
-        bucket = self.ubicar_valor(valor)
-        buckets_mayores = self.parametro - bucket - 1
-        buckets_mayores_o_iguales = self.parametro - bucket
-        probabilidad_minima = self.probabilidad_por_bucket * buckets_mayores
-        probabilidad_maxima = self.probabilidad_por_bucket * buckets_mayores_o_iguales
-        return (probabilidad_minima + probabilidad_maxima)/2
+        # como en el paper están definidas las fórmulas para estimar la probabilidad de que 'valor' sea MENOR (no mayor) que un
+        # elemento aleatorio de la distribución, definiremos estimate_lesser() y haremos esta función en base a ella
+        return 1 - self.estimate_lesser(valor) - self.estimate_equal(valor)
 
-    def ubicar_valor(self, valor):
+    def estimate_lesser(self, valor):
+        # extraido del paper
+        if self.entre_bordes(valor):
+            # caso A
+            return (self.ubicar(valor)+0.5)/self.parametro - self.delta/2
+        if self.igual_a_uno_no_extremo(valor):
+            # caso B1
+            return self.ubicar(valor)/self.parametro - self.delta/2
+        if self.igual_a_varios_no_extremos(valor):
+            # caso B2
+            return (self.ubicar(valor)-0.5)/self.parametro
+        if self.igual_a_varios_incluyendo_extremos(valor):
+            # caso C
+            if valor == self.bordes[0] == self.bordes[-1]:
+                return 0    # nadie es menor porque todos son iguales
+            if valor == self.bordes[0]:
+                return 0    # nadie es menor porque todos son mayores o iguales
+            if valor == self.bordes[-1]:
+                return 1 - (self.bordes.count(valor)-0.5)/self.parametro
+        if self.igual_a_un_extremo(valor):
+            # caso C1
+            if valor == self.bordes[0]:
+                return 0
+            if valor == self.bordes[1]:
+                return 1 - self.delta/2
+        if self.fuera_del_histograma(valor):
+            # caso D
+            if valor < self.bordes[0]:
+                return 0
+            if valor > self.bordes[-1]:
+                return 1
+            
+    def ubicar(self, valor):
         limite_inferior = max([limite for limite in self.bordes if limite <= valor])
-        return self.bordes.index(limite_inferior)
+        return float(self.bordes.index(limite_inferior))    # casteo a float porque se lo va a usar para aritmética flotante
 
 
 class EstimadorGrupo(Estimador):
@@ -129,3 +220,22 @@ class EstimadorGrupo(Estimador):
     def estimate_greater(self, valor):
         pass
 
+
+class EstimadorPerfecto(Estimador):
+    """ Para comparar. Responde siempre la respuesta correcta, mirando toda la tabla """
+    def build_struct(self):
+        pass
+
+    def estimate_equal(self, valor):
+        for v, ocurrencias in self.db.realizar_consulta(self.consulta_cuenta_ocurrencias):
+            if v == valor:
+                break
+        # 'ocurrencias' tiene la cantidad de ocurrencias del valor
+        return float(ocurrencias) / self.n_registros
+
+    def estimate_greater(self, valor):
+        n_mayores = 0
+        for (v, ) in self.db.realizar_consulta(self.consulta_desordenada):
+            if v > valor:
+                n_mayores += 1
+        return float(n_mayores) / self.n_registros
